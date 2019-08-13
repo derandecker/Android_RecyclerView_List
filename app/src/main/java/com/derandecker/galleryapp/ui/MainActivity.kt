fb02c69f18e09efcb201a22a439726938b7568df
Deran Decker <me@derandecker.com>
2019-08-13 12:36:12 -1000

    Update existing logic to work with RecyclerView. Project now compiles and runs.

--- app/src/main/java/com/derandecker/galleryapp/ui/MainActivity.kt
+++ app/src/main/java/com/derandecker/galleryapp/ui/MainActivity.kt
@@ -18,15 +18,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var ivList = mutableListOf<GroupOfImages>()
-
         var dImage: Drawable? = ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092)
+
+
         if (dImage != null) {
-            ivList.add(0, GroupOfImages(dImage, "Adventure Automobile"))
+            ivList.add(GroupOfImages(dImage, "Adventure Automobile"))
         }
 
         dImage = ContextCompat.getDrawable(this, R.drawable.abstract_abstract_expressionism_art_2505693)
         if (dImage != null) {
-            ivList.add(0, GroupOfImages(dImage, "Abstract Expressionism"))
+            ivList.add(GroupOfImages(dImage, "Abstract Expressionism"))
+        }
+
+        dImage = ContextCompat.getDrawable(this, R.drawable.aerial_photography_aerial_shot_aerial_view_2583847)
+        if (dImage != null) {
+            ivList.add(GroupOfImages(dImage, "Aerial Photography"))
+        }
+
+        dImage = ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285)
+        if (dImage != null) {
+            ivList.add(GroupOfImages(dImage, "Afterglow Beach Cliff"))
+        }
+
+        dImage = ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517)
+        if (dImage != null) {
+            ivList.add(GroupOfImages(dImage, "Alley and Buildings"))
+        }
+
+        dImage = ContextCompat.getDrawable(this, R.drawable.architectural_design_architecture_bridge_2540653)
+        if (dImage != null) {
+            ivList.add(GroupOfImages(dImage, "Architecture"))
+        }
+
+        dImage = ContextCompat.getDrawable(this, R.drawable.beautiful_breathtaking_canada_day_2526105)
+        if (dImage != null) {
+            ivList.add(GroupOfImages(dImage, "Breathtaking Canada"))
         }
 
 
