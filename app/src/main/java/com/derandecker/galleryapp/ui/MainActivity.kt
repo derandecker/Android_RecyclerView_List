f139d8ef8b4040656764e9f03949a1721d1c0956
Deran Decker <me@derandecker.com>
2019-08-13 12:25:29 -1000

    Attach List to Activity

--- app/src/main/java/com/derandecker/galleryapp/ui/MainActivity.kt
+++ app/src/main/java/com/derandecker/galleryapp/ui/MainActivity.kt
@@ -1,106 +1,61 @@
 package com.derandecker.galleryapp.ui
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.derandecker.galleryapp.R
+import com.derandecker.galleryapp.adapter.ImageListAdapter
+import com.derandecker.galleryapp.model.GroupOfImages
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var ivList = mutableListOf<ImageView>()
 
-        ivList.add(0, ImageView(this))
-        ivList[0].setImageDrawable(
-            ContextCompat.getDrawable(this,
-                R.drawable.abstract_abstract_expressionism_art_2505693
-            )
-        )
+        var ivList = mutableListOf<GroupOfImages>()
 
-        ivList.add(1, ImageView(this))
-        ivList[1].setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092)
-        )
-
-        ivList.add(2, ImageView(this))
-        ivList[2].setImageDrawable(
-            ContextCompat.getDrawable(this,
-                R.drawable.aerial_photography_aerial_shot_aerial_view_2583847
-            )
-        )
-
-        ivList.add(3, ImageView(this))
-        ivList[3].setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285)
-        )
-
-        ivList.add(4, ImageView(this))
-        ivList[4].setImageDrawable(
-            ContextCompat.getDrawable(this,
-                R.drawable.architectural_design_architecture_bridge_2540653
-            )
-        )
-
-        ivList.add(5, ImageView(this))
-        ivList[5].setImageDrawable(
-            ContextCompat.getDrawable(this,
-                R.drawable.beautiful_breathtaking_canada_day_2526105
-            )
-        )
-
-        ivList.add(6, ImageView(this))
-        ivList[6].setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.bloom_blossom_flora_2567011)
-        )
-
-        ivList.add(7, ImageView(this))
-        ivList[7].setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.close_up_colorful_colors_2529148)
-        )
-
-        ivList.add(8, ImageView(this))
-        ivList[8].setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168)
-        )
-
-        ivList.add(9, ImageView(this))
-        ivList[9].setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.colorful_colourful_houses_2501965)
-        )
-
-
-
-        val layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-
-        ivList.forEachIndexed { index, iv ->
-            iv.layoutParams = layoutParams
-            iv.adjustViewBounds = true
-            iv.setPadding(5, 5, 5, 5)
-            if (index % 2 == 0) {
-                left_column.addView(iv)
-            } else {
-                right_column.addView(iv)
-            }
-
-            iv_left_column.contentDescription = getString(R.string.alleyway)
+        var dImage: Drawable? = ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092)
+        if (dImage != null) {
+            ivList.add(0, GroupOfImages(dImage, "Adventure Automobile"))
         }
 
-//        <style name="ImageStyle">
-//        <item name="android:padding">5dp</item>
-//        <item name="android:layout_width">match_parent</item>
-//        <item name="android:layout_height">wrap_content</item>
-//        <item name="android:adjustViewBounds">true</item>
-//        </style>
+        dImage = ContextCompat.getDrawable(this, R.drawable.abstract_abstract_expressionism_art_2505693)
+        if (dImage != null) {
+            ivList.add(0, GroupOfImages(dImage, "Abstract Expressionism"))
+        }
+
+
+        rv_layout.setHasFixedSize(true)
+        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        val adapter = ImageListAdapter(ivList)
+        rv_layout.layoutManager = manager
+        rv_layout.adapter = adapter
+
+
+//      /CODE FROM PREVIOUS ITERATION OF PROJECT/
+//        val layoutParams = LinearLayout.LayoutParams(
+//            LinearLayout.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT
+//        )
+
+//      /CODE FROM PREVIOUS ITERATION OF PROJECT/
+//        ivList.forEachIndexed { index, iv ->
+//            iv.layoutParams = layoutParams
+//            iv.adjustViewBounds = true
+//            iv.setPadding(5, 5, 5, 5)
+//            if (index % 2 == 0) {
+//                left_column.addView(iv)
+//            } else {
+//                right_column.addView(iv)
+//            }
+//
+//            iv_left_column.contentDescription = getString(R.string.alleyway)
+//        }
 
     }
 }
